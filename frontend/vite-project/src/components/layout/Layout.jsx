1f8bfed8c6607d0a3c9640d0d220b812d67216d1
zeenathkousar <95568153+zeenathkousar@users.noreply.github.com>
2024-02-19 16:11:09 +0530

    added routing

--- frontend/vite-project/src/components/layout/Layout.jsx
+++ frontend/vite-project/src/components/layout/Layout.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { Helmet } from "react-helmet";
 
 export const Layout = ({ children }) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <Header />
       <main style={{ minHeight: "70vh" }}>{children}</main>
       <Footer />
